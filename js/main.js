1b36a26a400ec302f8e1dfc05d754cdd911c4750
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-19 23:41:17 +0200

    connect magnificentPopup, add about section styles

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.about-popup-link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
