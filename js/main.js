9f636b2e69246c37492f2ba38953969e78022087
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-21 12:47:23 +0200

    connect slick, add gallery section styles

--- js/main.js
+++ js/main.js
@@ -7,5 +7,33 @@ $(function () {
 		preloader: false,
 
 		fixedContentPos: false
+    });
+    
+    $('.gallery-slider').slick({
+        fade: true,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img class="slider-arrow slider-arrow-prev" src="images/icon-arrow-prev.svg" alt="arrow-prev" ></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img class="slider-arrow slider-arrow-next" src="images/icon-arrow-next.svg" alt="arrow-next" ></button>'
+        // responsive: [
+        //     {
+        //       breakpoint: 641,
+        //       settings: {
+        //         arrows: false
+        //       }
+        //     }
+        
+        // ]
+    });
+
+    $('.gallery-item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+
 	});
 });
